83027e4268df3432d193720cf073d98927b45d16
Chinmay <chinmayd@incubxperts.com>
2025-07-04 12:01:55 +0530

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace CameraSave
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+            FileResult photo = await MediaPicker.Default.CapturePhotoAsync();
+            
+        }
+    }
+
+}
